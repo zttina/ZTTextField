3bc7eecf405d7af43d83a684ee6c3a2615fe501f
ZT_Tina <351199191@qq.com>
2019-06-03 01:14:19 +0800

    实现自定义一框一数字的第一次提交

--- Example/ZTTextField/ZTViewController.m
+++ Example/ZTTextField/ZTViewController.m
@@ -11,6 +11,7 @@
 
 @interface ZTViewController ()
 
+@property (nonatomic, strong) ZTTextFieldsView *textFieldsView;
 @end
 
 @implementation ZTViewController
@@ -19,10 +20,30 @@
 {
     [super viewDidLoad];
     //外部调用
-    ZTTextFieldsView *view = [[ZTTextFieldsView alloc] initWithFrame:CGRectMake(100, 100, 100, 50)
-                                                           withColor:[UIColor redColor]];
-    [self.view addSubview:view];
+    self.textFieldsView = [[ZTTextFieldsView alloc] initWithFrame:CGRectMake(50, 100, (self.view.frame.size.width - 50*2), 80)
+                                                          textNum:5
+                                                            space:6];
+    [self.view addSubview:self.textFieldsView];
+    //调起键盘
+    [self.textFieldsView becomeFirstResponder];
+    //设置小文本框圆角
+    self.textFieldsView.cornerRadius = 10;
+    //设置小文本框字体
+    self.textFieldsView.font = [UIFont boldSystemFontOfSize:16];
+    //设置小文本框的placeHolder。
+    self.textFieldsView.placeHolder = @"这有五";
+    //设置小文本框的borderColor
+    self.textFieldsView.borderColor = [UIColor redColor];
+    //设置小文本框的borderWidth
+    self.textFieldsView.borderWidth = 0.5;
+    //设置小文本框的textColor
+    self.textFieldsView.textColor = [UIColor blueColor];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     
+    //键盘消失
+    [self.textFieldsView resignFirstResponder];
 }
 
 - (void)didReceiveMemoryWarning
